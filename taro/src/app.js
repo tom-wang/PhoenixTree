698f38ba7b0bca098a7f957fbcbda04a4e63de23
tom.wang <tom.wang.sz@qq.com>
2018-06-22 17:38:19 +0800

    整合redux

--- taro/src/app.js
+++ taro/src/app.js
@@ -1,7 +1,11 @@
 import Taro, { Component } from '@tarojs/taro'
+import { Provider } from '@tarojs/redux'
+import configStore from './store'
 import Index from './pages/index'
 
 import './app.scss'
+
+const store = configStore()
 
 class App extends Component {
   config = {
@@ -26,7 +30,9 @@ class App extends Component {
 
   render () {
     return (
-      <Index />
+      <Provider store={store}>
+        <Index />
+      </Provider>
     )
   }
 }
---
+++ taro/src/pages/index/index.js
@@ -0,0 +1,36 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+import './index.scss'
+
+import { add, minus, asyncAdd } from '../../actions/counter'
+
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add () {
+    dispatch(add())
+  },
+  dec () {
+    dispatch(minus())
+  },
+  asyncAdd () {
+    dispatch(asyncAdd())
+  }
+}))
+export default class Index extends Component {
+  config = {
+    navigationBarTitleText: '梧桐树下Plus'
+  }
+
+  render () {
+    return (
+      <View className='todo'>
+        <Button className='add_btn' onClick={this.props.add}>+</Button>
+        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
+        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
+        <View>{this.props.counter.num}</View>
+      </View>
+    )
+  }
+}
